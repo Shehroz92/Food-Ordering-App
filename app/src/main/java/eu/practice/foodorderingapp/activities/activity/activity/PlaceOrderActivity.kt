f517afc84cbb62165de15a489c3faf9b50301d04
Shehroz_Ali <muhammadalishehroz@gmail.com>
2024-09-08 04:34:44 +0500

    This is my 5th Commit

---
+++ app/src/main/java/eu/practice/foodorderingapp/activities/activity/activity/PlaceOrderActivity.kt
@@ -0,0 +1,14 @@
+package eu.practice.foodorderingapp.activities.activity.activity
+
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import eu.practice.foodorderingapp.R
+
+class PlaceOrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_place_order)
+    }
+}
