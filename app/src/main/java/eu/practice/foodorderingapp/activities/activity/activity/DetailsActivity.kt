ae0cf708aaf3f24f6f6ee25565cb4d2090d5533b
Shehroz_Ali <muhammadalishehroz@gmail.com>
2024-09-08 21:07:28 +0500

    This is my 5th Commit

---
+++ app/src/main/java/eu/practice/foodorderingapp/activities/activity/activity/DetailsActivity.kt
@@ -0,0 +1,13 @@
+package eu.practice.foodorderingapp.activities.activity.activity
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import eu.practice.foodorderingapp.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
