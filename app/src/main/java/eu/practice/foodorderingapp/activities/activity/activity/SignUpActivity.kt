05dd8529eaa34133ef2554011d48e6eaedae0440
Shehroz_Ali <muhammadalishehroz@gmail.com>
2024-11-01 18:12:11 +0500

    This is Food ordering app

--- app/src/main/java/eu/practice/foodorderingapp/activities/activity/activity/SignUpActivity.kt
+++ app/src/main/java/eu/practice/foodorderingapp/activities/activity/activity/SignUpActivity.kt
@@ -1,18 +1,25 @@
 package eu.practice.foodorderingapp.activities.activity.activity
 
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
+import eu.practice.foodorderingapp.R
 import eu.practice.foodorderingapp.activities.activity.models.UserModel
 import eu.practice.foodorderingapp.databinding.ActivitySigUpBinding
 
@@ -25,20 +32,27 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var username:String
     private lateinit var auth: FirebaseAuth
     private lateinit var database: DatabaseReference
-    private lateinit var googleSignInClient: GoogleSignInAccount
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     private val binding : ActivitySigUpBinding by lazy {
         ActivitySigUpBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val googleSignInOptions = GoogleSignInOptions.Builder(
+            GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(
+            getString(R.string.default_web_client_id)).requestEmail().build()
 
         // initialize Firebase auth
         auth = Firebase.auth
         // initialize Firebase database
         FirebaseApp.initializeApp(this)
         database = FirebaseDatabase.getInstance().reference
+        // initialize Firebase google
+        googleSignInClient = GoogleSignIn.getClient(this,googleSignInOptions)
 
 
 
@@ -47,6 +61,13 @@ class SignUpActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        binding.googleButton.setOnClickListener {
+            val signIntent = googleSignInClient.signInIntent
+            launcher.launch(signIntent)
+
+        }
+
 
         binding.buttonCreatAccount.setOnClickListener{
 
@@ -61,6 +82,29 @@ class SignUpActivity : AppCompatActivity() {
             }
         }
     }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result -> if (result.resultCode == Activity.RESULT_OK){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+        if (task.isSuccessful){
+            val account: GoogleSignInAccount? =task.result
+            val credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+            auth.signInWithCredential(credential).addOnCompleteListener {
+                task -> if (task.isSuccessful){
+                    startActivity(Intent(this,LoginActivity::class.java))
+                    finish()
+            } else {
+               Toast.makeText(this,"Sign in Failed",Toast.LENGTH_SHORT).show()
+            }
+            }
+        }
+    }else{
+        Toast.makeText(this,"Sign in Failed",Toast.LENGTH_SHORT).show()
+    }
+    }
+
+
+
 
     private fun createAccount(email: String, password: String) {
         auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
@@ -86,7 +130,7 @@ class SignUpActivity : AppCompatActivity() {
         val user = UserModel(username,email,password)
         val userId:String = FirebaseAuth.getInstance().currentUser!!.uid
         // save data to fire base data base
-        database.child("account").child(userId).setValue(user)
+        database.child("user").child(userId).setValue(user)
 
     }
 }
